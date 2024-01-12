f6a6fd92862ab152028a081aa86a05ed8df32993
Honey Patel <honeydpatel2523@gmail.com>
2024-01-12 10:21:16 +0530

    client/feat: add meeting form to app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,28 @@
+// src/App.tsx
+import React, { useState } from 'react';
+import MeetingForm, { FormDetails } from './components/MeetingForm';
+import './app.css'
+import { createCall } from './services/api';
 
-function App() {
+
+const App: React.FC = () => {
+  const [loading,setLoading]=useState(false)
+  const handleFormSubmit = (formData: FormDetails) => {
+    console.log('Form Data:', formData);
+    setLoading(true);
+    const response:Promise<string>=createCall(formData);
+    console.log(response);
+    setLoading(false);
+    
+  };
 
   return (
-    <h1>
-     Meeting Reminder
-    </h1>
-  )
-}
+    <>
+    <h1 className='title'>Send Meeting Reminder</h1>
+      <MeetingForm onSubmit={handleFormSubmit} />
+      {loading && <p>Loading...</p>}
+    </>
+  );
+};
 
-export default App
+export default App;
