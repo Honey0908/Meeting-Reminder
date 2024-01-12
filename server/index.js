053f30bb1746993ec0de59313d1778b8ac018eb6
Honey Patel <honeydpatel2523@gmail.com>
2024-01-12 19:24:58 +0530

    server/feat: add socket server - emit immidiate response of call status

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 const config = require('./config');
 const cors = require('cors');
-const router = require('./src/router')
-
+const socketio = require('socket.io');
+const { createRouter } = require('./src/router');
 
 var app = express();
 const port = config.port;
@@ -11,8 +11,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use('/', router);
-
-app.listen(port, () => {
+// app.use('/', router);
+const server = app.listen(port, () => {
     console.log('Server is running on port:', port);
 });
+
+const io = socketio(server, {
+    cors: {
+        origin: 'http://localhost:5173'
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log('New connection');
+})
+
+// Create a router with the passed io instance
+const router = createRouter(io);
+
+app.use('/', router);
--- server/src/router.js
+++ server/src/router.js
@@ -1,37 +1,52 @@
 const Router = require('express').Router;
-const { makeCall, handleAttendance, welcome, handleForwardedCall, handleVoiceMail } = require('./handler');
+const { makeCall, handleAttendance, welcome, handleForwardedCall } = require('./handler');
 
-const router = new Router();
 
-router.post('/make-call', async (req, res) => {
-    if (!req.body.meetingDetails || !req.body.to) {
-        res.send('invalid Meeting details or Contact number');
-    }
-    res.send(await makeCall(req.body.meetingDetails, req.body.to, req.body.from, res));
-})
+function createRouter(io) {
+    const router = new Router();
 
-router.post('/handle-gather', async (req, res) => {
-    res.type('text/xml');
-    res.send(await handleAttendance(req.body.Digits))
-})
+    router.post('/make-call', async (req, res) => {
+        if (!req.body.meetingDetails || !req.body.to) {
+            res.send('invalid Meeting details or Contact number');
+        }
+        res.send(await makeCall(req.body.meetingDetails, req.body.to, req.body.from, res));
+    })
 
-router.post('/welcome', (req, res) => {
-    res.type('text/xml');
-    res.send(welcome())
-})
+    router.post('/call-status', async (req, res) => {
+        io.emit('callStatus', req.body.CallStatus);
+        res.send(await makeCall(req.body.meetingDetails, req.body.to, req.body.from, res));
+    })
 
-router.post('/handle-forward', (req, res) => {
-    res.type('text/xml');
-    res.send(handleForwardedCall(req.body.DialCallStatus))
-})
+    router.post('/handle-gather', async (req, res) => {
+        const digit = req.body.Digits;
+        if (digit) {
+            io.emit('attendance', digit == "1" ? true : false);
+            res.type('text/xml');
+            res.send(await handleAttendance(digit))
+        }
+        // io.destroy()
+    })
 
-router.post('/handle-voicemail', (req, res) => {
-    const recordingUrl = req.body.RecordingUrl;
-    if (recordingUrl) {
-        handleVoiceMail(recordingUrl);
-        return res.sendStatus(200);
-    }
-    return res.sendStatus(500);
-});
+    router.post('/welcome', (req, res) => {
+        res.type('text/xml');
+        res.send(welcome())
+    })
 
-module.exports = router;
+    router.post('/handle-forward', (req, res) => {
+        res.type('text/xml');
+        res.send(handleForwardedCall(req.body.DialCallStatus))
+    })
+
+    router.post('/handle-voicemail', (req, res) => {
+        const recordingUrl = req.body.RecordingUrl;
+        if (recordingUrl) {
+            io.emit('recordingUrl', recordingUrl);
+            return res.sendStatus(200);
+        }
+        return res.sendStatus(500);
+    });
+
+    return router;
+}
+
+module.exports = { createRouter };
