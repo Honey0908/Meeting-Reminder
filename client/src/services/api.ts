7b6e1909edc3a09ccb9d65672741f8ff82bf2cba
Honey Patel <honeydpatel2523@gmail.com>
2024-01-15 14:59:09 +0530

    client/fix: solve typescript error for api res

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -2,13 +2,17 @@ import axios from 'axios';
 import { FormDetails } from '../components/MeetingForm';
 
 const API_BASE_URL = import.meta.env.VITE_SERVER_URL; 
+interface ApiResponse{
+  "sid":string;
+  [key: string]: any;
+}
 
-export const createCall = async (meetingDetails: FormDetails) => {
+export const createCall = async (meetingDetails: FormDetails):Promise<ApiResponse> => {
   try {
     const response = await axios({
       method: 'POST',
       url: `${API_BASE_URL}/make-call`,
-      data: { meetingDetails:meetingDetails.topic +"\n"+ meetingDetails.agenda ,to:`+91${meetingDetails.to}`},
+      data: { meetingDetails:meetingDetails.topic +"\n"+ meetingDetails.agenda ,to:`+91${meetingDetails.to}`,host_phoneNo:`+91${meetingDetails.host_phoneNo}`},
     });
     return response.data;
   } catch (error) {
