90d8fa0411d3ade21872a880a6a39219675cb65c
Honey Patel <honeydpatel2523@gmail.com>
2024-01-15 14:54:44 +0530

    client/feat: add refresh button -change socket setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,18 +5,19 @@ import './app.css'
 import { createCall } from './services/api';
 import { io } from 'socket.io-client';
 
-const socket = io(import.meta.env.VITE_SERVER_URL);
-
 const App: React.FC = () => {
-
   const [loading, setLoading] = useState(false);
-  const [callInitiated,setCallInitiated]=useState(false);
+  const [callInitiated, setCallInitiated] = useState(false);
   const [attendance, setAttendance] = useState<boolean | undefined>(undefined);
   const [recordingUrl, setRecordingUrl] = useState("");
   const [callStatus, setCallStatus] = useState("");
 
   useEffect(() => {
-    socket.on('connect', () => console.log('Connected to server'));
+    const socket = io(import.meta.env.VITE_SERVER_URL);
+
+    socket.on('connect', () => {
+      console.log("connected");
+    });
 
     socket.on('recordingUrl', (recordingUrl) => {
       setRecordingUrl(recordingUrl);
@@ -27,41 +28,53 @@ const App: React.FC = () => {
     });
 
     socket.on('callStatus', (callStatus) => {
-      setCallStatus(callStatus)
+      console.log("callStatus");
+      setCallStatus(callStatus);
     });
 
     return () => {
-      socket.disconnect();
+      socket?.disconnect();
     };
-
-  }, [socket])
-
+  }, []); 
 
   const handleFormSubmit = async (formData: FormDetails) => {
     setLoading(true);
-    const response: Promise<object> = await createCall(formData);
-    if(response?.sid!){
+    const response = await createCall(formData);
+    if (response?.sid) {
       setCallInitiated(true);
     }
     setLoading(false);
   };
 
+  const handleRefresh = () => {
+    setCallInitiated(false);
+    setAttendance(undefined);
+    setRecordingUrl("");
+    setCallStatus("");
+  };
+
   return (
     <>
       <h1 className='title'>Send Meeting Reminder</h1>
-      <MeetingForm onSubmit={handleFormSubmit} />
-      {loading  ? (
+      <MeetingForm callInitiated={callInitiated} onRefresh={handleRefresh} onSubmit={handleFormSubmit} />
+      {loading ? (
         <p>Loading...</p>
-      ) : (
-        callInitiated ? 
+      ) : callInitiated ? (
         <>
-          {!callStatus && <p>Calling...</p>}
-          {attendance == undefined ? <p>Attendance: Unset</p> : attendance ? <p>User has Confirmed</p> : <p>User has declined</p>}
+          {!callStatus && <p>Call ongoing...</p>}
+          {attendance === undefined ? (
+            <p>Attendance: Unset</p>
+          ) : attendance ? (
+            <p>User has Confirmed</p>
+          ) : (
+            <p>User has declined</p>
+          )}
           {recordingUrl && <a href={recordingUrl}>Voice Mail</a>}
           {callStatus && <p>Call Status: {callStatus}</p>}
-        </> :<></>
+        </>
+      ) : (
+        <></>
       )}
-
     </>
   );
 };
--- client/src/components/MeetingForm.tsx
+++ client/src/components/MeetingForm.tsx
@@ -3,22 +3,23 @@ import React, { useState } from 'react';
 
 interface MeetingFormProps {
   onSubmit: (formData: FormDetails) => void;
-  callInitiated:boolean;
+  callInitiated: boolean;
+  onRefresh: () => void;
 }
 
 export interface FormDetails {
   topic: string;
   agenda: string;
   to: string;
-  host_phoneNo:string;
+  host_phoneNo: string;
 }
 
-const MeetingForm: React.FC<MeetingFormProps> = ({ onSubmit,callInitiated }) => {
+const MeetingForm: React.FC<MeetingFormProps> = ({ onSubmit, callInitiated, onRefresh }) => {
   const [formData, setFormData] = useState<FormDetails>({
     topic: '',
     agenda: '',
     to: '',
-    host_phoneNo:''
+    host_phoneNo: ''
   });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -28,30 +29,44 @@ const MeetingForm: React.FC<MeetingFormProps> = ({ onSubmit,callInitiated }) => 
     });
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     onSubmit(formData);
   };
 
+  const handleRefresh = () => {
+    setFormData({ topic: '', agenda: '', to: '', host_phoneNo: '' });
+    onRefresh();
+  };
+
   return (
-    <form onSubmit={handleSubmit} className="meeting-form">
-      <label htmlFor='topic' className="form-label">Meeting Topic:</label>
+    <>
+      <form onSubmit={handleSubmit} className="meeting-form">
+        <label htmlFor='topic' className="form-label">Meeting Topic:</label>
         <input disabled={callInitiated} type="text" name="topic" value={formData.topic} onChange={handleChange} required />
 
-      <label className="form-label" htmlFor='agenda'> Agenda:</label>
-        <textarea disabled={callInitiated} name="agenda" value={formData.agenda} onChange={handleChange} required rows={10} cols={70}/>
+        <label className="form-label" htmlFor='agenda'> Agenda:</label>
+        <textarea disabled={callInitiated} name="agenda" value={formData.agenda} onChange={handleChange} required rows={10} cols={70} />
 
-      <label className="form-label" htmlFor='to'> Phone Number: </label>
-        <input disabled={callInitiated} type="tel" name="to" pattern="[0-9]{10}" value={formData.to} onChange={handleChange} required/>
-        
-      <label className="form-label" htmlFor='host_phoneNo'> Host Phone Number: </label>
-        <input disabled={callInitiated} type="tel" name="host_phoneNo" pattern="[0-9]{10}" value={formData.host_phoneNo} onChange={handleChange} required/>
+        <label className="form-label" htmlFor='to'> Phone Number: </label>
+        <input disabled={callInitiated} type="tel" name="to" pattern="[0-9]{10}" value={formData.to} onChange={handleChange} required />
+
+        <label className="form-label" htmlFor='host_phoneNo'> Host Phone Number: </label>
+        <input disabled={callInitiated} type="tel" name="host_phoneNo" pattern="[0-9]{10}" value={formData.host_phoneNo} onChange={handleChange} required />
         <small className="form-hint">Enter a 10-digit phone number without spaces or dashes.</small>
 
-      <button disabled={callInitiated} type="submit" className="submit-button">
-        Call
-      </button>
-    </form>
+        <button disabled={callInitiated} type="submit" className="submit-button">
+          Call
+        </button>
+      </form>
+      <div className='refresh-button-div'>
+        <button title='refresh' className='refresh-button' onClick={handleRefresh}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" height={25} width={25}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+          </svg>
+        </button>
+      </div>
+    </>
   );
 };
 
