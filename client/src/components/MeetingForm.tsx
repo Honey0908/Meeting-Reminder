c3ce61088ce7160c5279df3fa18bef3ae82d4bdc
Honey Patel <honeydpatel2523@gmail.com>
2024-01-15 12:52:18 +0530

    client/feat: add host ph. field & disabled options

--- client/src/components/MeetingForm.tsx
+++ client/src/components/MeetingForm.tsx
@@ -3,19 +3,22 @@ import React, { useState } from 'react';
 
 interface MeetingFormProps {
   onSubmit: (formData: FormDetails) => void;
+  callInitiated:boolean;
 }
 
 export interface FormDetails {
   topic: string;
   agenda: string;
   to: string;
+  host_phoneNo:string;
 }
 
-const MeetingForm: React.FC<MeetingFormProps> = ({ onSubmit }) => {
+const MeetingForm: React.FC<MeetingFormProps> = ({ onSubmit,callInitiated }) => {
   const [formData, setFormData] = useState<FormDetails>({
     topic: '',
     agenda: '',
     to: '',
+    host_phoneNo:''
   });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -33,16 +36,19 @@ const MeetingForm: React.FC<MeetingFormProps> = ({ onSubmit }) => {
   return (
     <form onSubmit={handleSubmit} className="meeting-form">
       <label htmlFor='topic' className="form-label">Meeting Topic:</label>
-        <input type="text" name="topic" value={formData.topic} onChange={handleChange} required />
+        <input disabled={callInitiated} type="text" name="topic" value={formData.topic} onChange={handleChange} required />
 
       <label className="form-label" htmlFor='agenda'> Agenda:</label>
-        <textarea name="agenda" value={formData.agenda} onChange={handleChange} required rows={10} cols={70}/>
+        <textarea disabled={callInitiated} name="agenda" value={formData.agenda} onChange={handleChange} required rows={10} cols={70}/>
 
       <label className="form-label" htmlFor='to'> Phone Number: </label>
-        <input type="tel" name="to" pattern="[0-9]{10}" value={formData.to} onChange={handleChange} required/>
+        <input disabled={callInitiated} type="tel" name="to" pattern="[0-9]{10}" value={formData.to} onChange={handleChange} required/>
+        
+      <label className="form-label" htmlFor='host_phoneNo'> Host Phone Number: </label>
+        <input disabled={callInitiated} type="tel" name="host_phoneNo" pattern="[0-9]{10}" value={formData.host_phoneNo} onChange={handleChange} required/>
         <small className="form-hint">Enter a 10-digit phone number without spaces or dashes.</small>
 
-      <button type="submit" className="submit-button">
+      <button disabled={callInitiated} type="submit" className="submit-button">
         Call
       </button>
     </form>
